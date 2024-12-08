18f7faf0c66ce48519cc60d5f6a312a2c41fea09
alan2535 <85979658+alan2535@users.noreply.github.com>
2024-12-09 03:10:24 +0800

    1

---
+++ Assets/Car.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Car : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+    void Start()
+    {
+    }
+    void Update()
+    {
+        //(新增)取得滑動長度
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition; //將點擊滑鼠時的座標記錄起來
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition; //將放開滑鼠時的座標記錄起來
+            float swipeLength = endPos.x - startPos.x;
+            speed = swipeLength / 500.0f; //把滑動長度轉換成初始移動速度
+            GetComponent<AudioSource>().Play();
+        }
+        transform.Translate(speed, 0, 0);
+        speed *= 0.98f;
+    }
+}
